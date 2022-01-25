4113f5813451b7c76d46c3690ba77cea596fb73e
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-01-25 14:31:09 +0900

    영구 번호에 따라 상세페이지 표시

--- src/Detail.js
+++ src/Detail.js
@@ -6,6 +6,10 @@ function Detail(props) {
 
   let history = useHistory();
   let { id } = useParams();
+  
+  let item_id = props.shoes.find(function(shoes) {
+    return shoes.id == id
+  })
 
   return (
   <div className="container">
@@ -14,9 +18,9 @@ function Detail(props) {
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
       </div>
       <div className="col-md-6 mt-4">
-        <h4 className="pt-5">{props.shoes[id].title}</h4>
-        <p>{props.shoes[id].content}</p>
-        <p>{props.shoes[id].price}원</p>
+        <h4 className="pt-5">{item_id.title}</h4>
+        <p>{item_id.content}</p>
+        <p>{item_id.price}원</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
