fe04e095a4c5b66996e9c4d63450dd16f8a45914
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-01-25 14:37:16 +0900

    styled-component 사용

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,16 @@
-
+/*eslint-disable*/ 
 import React, { useState } from 'react';
 import { useHistory,useParams } from 'react-router-dom';
+import styled from 'styled-components'
+let 박스 = styled.div` 
+            padding : 20px; 
+          `; 
+let 제목 = styled.h4` 
+            font-size : 25px; 
+            color : ${ props => props.색상 }; 
+          `;
+
+
 
 function Detail(props) {
 
@@ -13,6 +23,11 @@ function Detail(props) {
 
   return (
   <div className="container">
+    <박스> 
+      <제목 색상="blue">안녕하세요1</제목> 
+      <제목 색상={'red'}>안녕하세요2</제목>
+    </박스>
+
     <div className="row">
       <div  className="col-md-6">
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
